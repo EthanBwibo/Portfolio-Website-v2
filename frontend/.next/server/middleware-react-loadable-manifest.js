1e86f2fee7caef500754d0bc081c6c8f820503c0
ethanbwibo-Strath <enbwibo@gmail.com>
2026-03-03 09:27:17 +0300

    untrack .next folder

--- frontend/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app/page.js -> @/components/CustomCursor\":{\"id\":\"app/page.js -> @/components/CustomCursor\",\"files\":[\"static/chunks/_app-pages-browser_src_components_CustomCursor_jsx.js\"]}}"
